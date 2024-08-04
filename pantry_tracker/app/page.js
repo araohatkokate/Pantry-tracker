df4c80303c3bfaba14a58ad31ce46b5a1d82b5b2
araohatkokate <araohatk@gmail.com>
2024-08-04 07:19:32 -0500

    changed positioning of title

--- pantry_tracker/app/page.js
+++ pantry_tracker/app/page.js
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import { useState, useEffect } from 'react'
 import { firestore } from "@/firebase"
-import { Box, Typography } from "@mui/material";
-import { collection, getDoc, getDocs, query } from "firebase/firestore";
+import { Box, Modal, Typography } from "@mui/material";
+import { collection, getDoc, getDocs, query, setDoc } from "firebase/firestore";
 
 export default function Home() {
   const [inventory, setinventory] = useState([])
@@ -26,31 +26,53 @@ export default function Home() {
     console.log(inventorylist)
   }
 
+  const additem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity+1})
+    }
+      
+    else{
+        await setDoc(docRef, {quantity:1})
+    }
+    await updateinventory()
+  }
+  
   const removeitem = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item)
-    const docsnap = await getDoc(docRef)
+    const docSnap = await getDoc(docRef)
 
-    if(docsnap.exists()){
-      const {quantity} = docsnap.data()
+    if(docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if(quantity===1){
+        await deleteDoc(docRef)
+      }
+
+      else{
+        await setDoc(docRef, {quantity: quantity-1})
+      }
     }
+
+    await updateinventory()
   }
 
   useEffect (()=>{
     updateinventory()
   }, [])
 
+  const handleOpen = () => setopen(true)
+  const handClose = () => setopen(false)
+
   return (
-  <Box>
-    <Typography variant="h1">Pantry Tracker</Typography>
-    {inventory.forEach((item)=> {
-      console.log(item)
-      return (
-      <Box>
-      {item.name}
-      {item.count}
+  <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" gap={2}>
+    <Modal open={open} onClose={handClose}>
+      <Box position="absolute" top="50%" left="50%" transform="translate(-50%,-50%)">
       </Box>
-      )
-    })}
+    </Modal>
+    <Typography variant="h1">Pantry Tracker</Typography>
   </Box> 
   )
 }
