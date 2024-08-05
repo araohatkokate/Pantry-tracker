7abbe22720df2d447d0eb3da68f9d8d7d2326c7a
araohatkokate <araohatk@gmail.com>
2024-08-05 02:27:39 -0500

    implementing firebase authentication and setting up local environment variables

--- pantry_tracker/app/firebase/firebase.js
+++ pantry_tracker/app/firebase/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCa5qCsdLLZTYdAYwjLuKPR35jY_0rPipQ",
-  authDomain: "pantry-tracker-7bb35.firebaseapp.com",
-  projectId: "pantry-tracker-7bb35",
-  storageBucket: "pantry-tracker-7bb35.appspot.com",
-  messagingSenderId: "460011347116",
-  appId: "1:460011347116:web:8131401c81e0d5569a9309",
-  measurementId: "G-7XK2TYRRH5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSENGER_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
