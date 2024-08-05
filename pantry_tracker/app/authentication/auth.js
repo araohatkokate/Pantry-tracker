8459059bc1bd6d716a34a6e40411b52d3d9af8dc
araohatkokate <araohatk@gmail.com>
2024-08-05 05:46:51 -0500

    fully functional google sign in implemented

--- pantry_tracker/app/authentication/auth.js
+++ pantry_tracker/app/authentication/auth.js
@@ -1,9 +1,11 @@
 // auth.js
 import { auth, GoogleAuthProvider, signInWithPopup, signOut } from "@/app/firebase/firebaseconfig";
 
+const provider = new GoogleAuthProvider();
+
 export const signInWithGoogle = async () => {
   try {
-    const result = await signInWithPopup(auth, GoogleAuthProvider);
+    const result = await signInWithPopup(auth, provider);
     // The signed-in user info.
     const user = result.user;
     console.log('User signed in: ', user);
