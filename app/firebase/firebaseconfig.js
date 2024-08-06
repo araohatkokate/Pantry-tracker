78bd29e741189c035b34bedd41c66b22e8124809
araohatkokate <araohatk@gmail.com>
2024-08-06 00:41:27 -0500

    changed firebase configuration

--- app/firebase/firebaseconfig.js
+++ app/firebase/firebaseconfig.js
@@ -2,19 +2,16 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
  
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyCa5qCsdLLZTYdAYwjLuKPR35jY_0rPipQ",
-  authDomain: "pantry-tracker-7bb35.firebaseapp.com",
-  projectId: "pantry-tracker-7bb35",
-  storageBucket: "pantry-tracker-7bb35.appspot.com",
-  messagingSenderId: "460011347116",
-  appId: "1:460011347116:web:8131401c81e0d5569a9309",
-  measurementId: "G-7XK2TYRRH5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
  
 // Initialize Firebase
@@ -22,4 +19,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const firestore = getFirestore(app);
 const auth = getAuth(app);
  
-export {app, auth, GoogleAuthProvider, signInWithPopup, signOut, firestore};
+export { app, auth, GoogleAuthProvider, signInWithPopup, signOut, firestore };
